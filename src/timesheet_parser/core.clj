cc59e525d363d74f02ae4b3a96e4e0d6305a0b80
jgoodhcg <jgoodhcg@gmail.com>
2017-01-22 18:31:53 -0500

    slurp and parse-csv from resources dir

--- src/timesheet_parser/core.clj
+++ src/timesheet_parser/core.clj
@@ -1,7 +1,12 @@
 (ns timesheet-parser.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure-csv.core :refer [parse-csv]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(-> "/timesheet-parser/resources/timesheet.csv"
+    slurp
+    parse-csv)
